c777e1d07e14b0b18fddae00505358299d45e6d3
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-06-10 13:03:51 +0600

    new crypto project setup in react and talwind project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center font-semibold bg-purple-800 text-5xl">I am swapnil ahmed shishir</h1>
+    </div>
+  )
+}
+
+export default App
